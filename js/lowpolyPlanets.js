0683aa9b0560b964bc1b26b1f461d08b1b3ea600
skyguy99 <coolsnt@gmail.com>
2016-05-16 10:17:08 -0700

    may16

--- js/lowpolyPlanets.js
+++ js/lowpolyPlanets.js
@@ -1,5 +1,5 @@
 // set the scene size
-    var parent, renderer, scene, camera, controls, pointLight;
+    var superparent, parent, renderer, scene, camera, controls, pointLight;
         
     //Call startup funcs
     init();
@@ -7,15 +7,15 @@
         
     function init() {  
         
-	var WIDTH = 200,
-	    HEIGHT = 200;
+	var WIDTH = window.innerWidth;
+	    HEIGHT = window.innerHeight;
         // set some camera attributes
 	var VIEW_ANGLE = 45,
 	    ASPECT = WIDTH / HEIGHT,
 	    NEAR = 0.1,
 	    FAR = 10000;
 	// get the DOM element to attach to
-	var $container = $('#btn1');
+	var $container = $('.wrapper2');
 	// Define all variables --------------------------------------------------------------
 	renderer = new THREE.WebGLRenderer({alpha: true});
 	renderer.setSize(WIDTH, HEIGHT);
@@ -27,10 +27,7 @@
 	scene = new THREE.Scene();
 	
 	// camera
-	camera = new THREE.PerspectiveCamera(  VIEW_ANGLE,
-	                                ASPECT,
-	                                NEAR,
-	                                FAR );
+	camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 	camera.position.set( 12, -150, 30 );
 	// controls
 	controls = new THREE.OrbitControls( camera, renderer.domElement); //This is crucial for forms working
@@ -39,12 +36,8 @@
         
         controls.update();
 	
-	// axes
-	//scene.add( new THREE.AxisHelper( 20 ) );
 	// geometry
-	var geometry = new THREE.BoxGeometry( 2, 2, 2 );
-	var planet = new THREE.SphereGeometry(5, 16, 16);
-    var moon = new THREE.SphereGeometry(1,16,16);
+	var sun = new THREE.IcosahedronGeometry(18,2);
         
     var planet2 = new THREE.IcosahedronGeometry(8,1);
     var moon2 = new THREE.IcosahedronGeometry(1.5, 0);
@@ -67,8 +60,13 @@
         
 	
 	// parent
+    superparent = new THREE.Object3D();   
+    scene.add(superparent);
+        
 	parent = new THREE.Object3D();
-	scene.add( parent );
+    parent.rotation.z = 5* Math.PI / 3;  
+        
+	superparent.add( parent );
 	// pivots
 	var pivot1 = new THREE.Object3D();
 	var pivot2 = new THREE.Object3D();
@@ -84,14 +82,20 @@
 	// mesh
 	var mesh1 = new THREE.Mesh( planet2, defaultMaterial );
 	var mesh2 = new THREE.Mesh( moon2, defaultMaterial );
-    var mesh4 = new THREE.Mesh(ring, material2);    
+    var mesh3 = new THREE.Mesh(ring, material2);  
+    
         
-    mesh4.position.y = 0;    
+       
 	mesh1.position.y = 0;
 	mesh2.position.y = 14; //dist from center
+    mesh3.position.y = 0;    
+        
 	pivot1.add( mesh1 );
 	pivot2.add( mesh2 );
-    scene.add(mesh4);
+    scene.add(mesh3);
+    
+    //stars
+    scene.add(new createParticleSystem());
         
     pointLight = new THREE.PointLight( 0xFFFFFF );
 	// set its position
@@ -101,6 +105,46 @@
 	// add to the scene
 	scene.add(pointLight);
 	
+}
+function createParticleSystem() {
+     
+    // The number of particles in a particle system is not easily changed.
+    var particleCount = 2000;
+     
+    // Particles are just individual vertices in a geometry
+    // Create the geometry that will hold all of the vertices
+    var particles = new THREE.IcosahedronGeometry(1, 0);
+ 
+    // Create the vertices and add them to the particles geometry
+    for (var p = 0; p < particleCount; p++) {
+     
+        // This will create all the vertices in a range of -200 to 200 in all directions
+        var x = Math.random() * 400 - 200;
+        var y = Math.random() * 400 - 200;
+        var z = Math.random() * 400 - 200;
+               
+        // Create the vertex
+        var particle = new THREE.Vector3(x, y, z);
+         
+        // Add the vertex to the geometry
+        particles.vertices.push(particle);
+    }
+ 
+    // Create the material that will be used to render each vertex of the geometry
+    var particleMaterial = new THREE.PointsMaterial(
+            {color: 0xffffff, 
+             size: 4,
+             shading: THREE.FlatShading,
+             blending: THREE.AdditiveBlending,
+             transparent: true,
+            });
+    
+    var partMaterial2 = new THREE.MeshPhongMaterial({ color: 0xffffff, shading: THREE.FlatShading, vertexColors: THREE.VertexColors, shininess: 0	} );
+      
+    // Create the particle system
+    particleSystem = new THREE.Points(particles, particleMaterial);
+ 
+    return particleSystem;  
 }
 function animate() {
 	requestAnimationFrame( animate );
